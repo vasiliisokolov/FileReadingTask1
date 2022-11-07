d810fd54fec011e952aaebfa81dcab8d5341e466
VasiliiSokolov <vasilysokolov@mail.ru>
2022-11-07 18:35:43 +0300

    Добавьте файлы проекта.

---
+++ FileReadingTask1.cpp
@@ -0,0 +1,25 @@
+﻿#include <fstream>
+#include <iostream>
+
+int main()
+{
+    std::ifstream book;
+    book.open("C:\\Users\\Василий\\source\\repos\\FileReadingTask1\\words.txt");
+    std::string temp;
+    std::string searchOne;
+    std::cout << "Entert the word to search: ";
+    std::cin >> searchOne;
+    int counter = 0;
+    do
+    {
+        book >> temp;
+        if (temp == searchOne)
+        {
+            counter++;
+        }
+        //std::cout << temp << std::endl;
+    } while (temp != "End");
+    book.close();
+    std::cout << "We found " << counter << " matches!" << std::endl;
+}
+
